ef6a004ee5e453dbc590e841eca998dfbfd47c25
comody <comody224@gmail.com>
2023-10-15 08:57:54 +0900

    [Chap2][ENH]init

---
+++ awsspring/src/main/kotlin/com/demo/awsspring/AwsspringApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.awsspring
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class AwsspringApplication
+
+fun main(args: Array<String>) {
+	runApplication<AwsspringApplication>(*args)
+}
