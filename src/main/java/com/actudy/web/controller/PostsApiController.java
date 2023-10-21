42c3a3f529f66101e827ce5768ea312426255628
experjun <realfunlady@gmail.com>
2023-10-21 12:58:29 +0900

    feat: add a CRUD example based on jpa and mustache

---
+++ src/main/java/com/actudy/web/controller/PostsApiController.java
@@ -0,0 +1,36 @@
+package com.actudy.web.controller;
+
+import com.actudy.web.dto.PostsResponseDto;
+import com.actudy.web.dto.PostsSaveRequestDto;
+import com.actudy.web.dto.PostsUpdateRequestDto;
+import com.actudy.web.service.PostsService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+public class PostsApiController {
+
+    private final PostsService postsService;
+
+    @PostMapping("/api/v1/posts")
+    public Long save(@RequestBody PostsSaveRequestDto requetsDto) {
+        return postsService.save(requetsDto);
+    }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id) {
+        return postsService.findById(id);
+    }
+
+    @DeleteMapping("/api/v1/posts/{id}")
+    public Long delete(@PathVariable Long id) {
+        postsService.delete(id);
+        return id;
+    }
+}
